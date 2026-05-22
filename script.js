a2971e8b2d347c9b58acbfe6466cfa4cf729b828
Ivan <unicornixtv@gmail.com>
2026-05-22 17:03:16 +0300

    добавлена отрисовка шарика и его движение, без коллизии с границами поля

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const canvas = document.getElementById('arkan');
 const ctx = canvas.getContext('2d');
 
+const ball_radius = 10;
+let ballX = canvas.width / 2;
+let ballY = canvas.height - 40;
+let dX = 3;
+let dY = -3;
 
 const paddle = {
     x: 490,
@@ -25,10 +30,35 @@ function drawPaddle() {
 
     ctx.closePath();
 }
+
+function drawBall() {
+
+    ctx.beginPath();
+
+    ctx.arc(
+        ballX,
+        ballY,
+        ball_radius,
+        0,
+        Math.PI * 2
+
+    );
+
+    ctx.fillStyle = '#ffffff';
+    ctx.fill();
+
+    ctx.closePath();
+
+}
+
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     drawPaddle();
+    drawBall();
+
+    ballX += dX;
+    ballY += dY;
 
     requestAnimationFrame(draw);
 }
